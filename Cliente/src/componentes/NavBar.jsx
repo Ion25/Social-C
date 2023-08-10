b6f5a70de29f398f08cd02728e102ca5707fb869
Ion25 <65067718+Ion25@users.noreply.github.com>
2023-08-09 23:21:09 -0500

    Update NavBar.jsx

--- Cliente/src/componentes/NavBar.jsx
+++ Cliente/src/componentes/NavBar.jsx
@@ -1,13 +1,18 @@
+import { useContext } from "react";
 import { Container, Nav, Navbar, Stack } from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AutorContext } from "../context/AutorContext";
+
 const NavBar = () => {
+    const {user} = useContext(AutorContext);
+
     return( 
         <Navbar bg="dark" className="mb-4" style={{height:"3.75rem"}}>
             <Container>
                 <h2>
                     <Link to="/" className="name-titulo">Social-C</Link>
                 </h2>
-                <span className="mensaje-conectado">En linea </span>
+                <span className="mensaje-conectado">Logeado como {user?.name} </span>
                 <nav>
                     <Stack direction="horizontal" gap="3">
                         <Link to="/login" className="login">Login</Link>
